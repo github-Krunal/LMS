46eba87d75d16916ea017696189af57cbf404ef7
githubkrunal <githubkrunal@gmail.com>
2025-09-21 12:38:55 +0530

    registration page create

--- src/app/components/authentication/registration/registration.component.ts
+++ src/app/components/authentication/registration/registration.component.ts
@@ -1,9 +1,10 @@
+import { RouterModule } from '@angular/router';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-registration',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './registration.component.html',
   styleUrl: './registration.component.scss'
 })
