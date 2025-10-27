8b5ac458c2a07d04c1f1d017264dc57581f334c2
githubkrunal <githubkrunal@gmail.com>
2025-10-27 20:38:05 +0530

    course api call

--- src/app/components/instructor/inx-courses/inx-courses.component.ts
+++ src/app/components/instructor/inx-courses/inx-courses.component.ts
@@ -2,6 +2,8 @@ import { AngularModule } from './../../../modules/angular/angular.module';
 import { Component } from '@angular/core';
 import { GlobalModule } from '../../../modules/angular/global.module';
 import { RepositoryModule } from '../../../modules/angular/reporsitory.module';
+import { ApiService } from '../../../services/api.service';
+import { Course } from '../../../model/course.interface';
 
 @Component({
   selector: 'app-inx-courses',
@@ -13,11 +15,24 @@ import { RepositoryModule } from '../../../modules/angular/reporsitory.module';
 export class InxCoursesComponent {
   protected isOpenSideNav: boolean = false;
   protected repositoryID:string="TutorCourses";
+  protected coursesList:Course[]=[]
+
+  constructor(private apiService:ApiService){}
+
+  ngOnInit(): void {
+    this.getAllCourses();
+  }
 
   protected onCourseClick() {
     this.isOpenSideNav = true;
   }
   protected closeSidePanel() {
     this.isOpenSideNav = false;
+  }
+
+  private getAllCourses(){
+    this.apiService.getAllRecords(this.repositoryID).subscribe((courses:Course[])=>{
+      this.coursesList=courses;
+    })
   }
 }
